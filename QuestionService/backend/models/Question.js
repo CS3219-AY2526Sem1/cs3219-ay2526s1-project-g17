ad7d8fa55fe362b02b616291126e8430affe7f1e
ywllowsensor <isyazani@gmail.com>
2025-09-29 22:05:54 +0800

    feat: Create question model

---
+++ QuestionService/backend/models/Question.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const questionSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+        },
+        question: {
+            type: String,
+            required: true,
+        },
+        difficulty: {
+            type: String,
+            enum: ['Beginner','Intermediate','Advanced'],
+            required: true,
+        },
+        topics: {
+            type: [String],
+            required: true,
+        },
+    },
+    { timestamps: true }
+);
+
+const
