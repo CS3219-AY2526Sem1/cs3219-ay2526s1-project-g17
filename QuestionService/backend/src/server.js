6677c4eebff9d5fdecb31fc588bbb578929e1dd2
ywllowsensor <isyazani@gmail.com>
2025-09-29 22:05:42 +0800

    style: Remove redundant code

--- QuestionService/backend/src/server.js
+++ QuestionService/backend/src/server.js
@@ -6,7 +6,7 @@ import { connectDB } from "../config/db.js";
 import rateLimiter from "../middleware/rateLimiter.js";
 
 const app = express();
-const test = dotenv.config()
+dotenv.config()
 const PORT = process.env.PORT || 5001;
 
 connectDB();
@@ -16,10 +16,6 @@ app.use(express.json());
 
 app.use(rateLimiter);
 
-// app.use((req, res, next)=> {
-//     console.log(`Req method is ${req.method} and Req URL is ${req.url}`);
-//     next();
-// })
 
 app.use("/api/notes", routes);
 connectDB(() => {
