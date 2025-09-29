1573a57b0b275b94d0cedb903342e7377e9b52a8
ywllowsensor <isyazani@gmail.com>
2025-09-29 18:44:22 +0800

    chore: finish express + mongodb tutorial

--- QuestionService/backend/src/server.js
+++ QuestionService/backend/src/server.js
@@ -1,10 +1,29 @@
 import express from "express";
+import dotenv from "dotenv"
+
 import routes from "./routes/route.js"
+import { connectDB } from "../config/db.js";
+import rateLimiter from "../middleware/rateLimiter.js";
 
 const app = express();
+const test = dotenv.config()
+const PORT = process.env.PORT || 5001;
 
-app.use("/api/notes", routes)
+connectDB();
 
-app.listen(5001, () => {
-    console.log("server started on port: 5001");
+// middleware
+app.use(express.json());
+
+app.use(rateLimiter);
+
+// app.use((req, res, next)=> {
+//     console.log(`Req method is ${req.method} and Req URL is ${req.url}`);
+//     next();
+// })
+
+app.use("/api/notes", routes);
+connectDB(() => {
+    app.listen(PORT, () => {
+        console.log("server started on port: 5001");
+    });
 });
