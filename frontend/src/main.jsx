7cd4beaad127de3341eff9ac8b71244b2abc7500
MinkhantEain <khantkyaw.pkk@gmail.com>
2025-10-05 17:35:38 +0800

    setup routing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
+import "./index.css";
+import App from "./app";
+import MatchingPage from "./matching-service/pages/matching_page";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = document.getElementById("root");
+
+const router = createBrowserRouter([
+  { path: "/", element: <App /> },
+  { path: "/match", element: <MatchingPage /> },
+]);
+
+ReactDOM.createRoot(root).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+    {/* <BrowserRouter>
+      <App />
+    </BrowserRouter> */}
+  </React.StrictMode>
+);
