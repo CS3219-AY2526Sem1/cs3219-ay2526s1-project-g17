53047ad74807b4a2a21010d2faa3225a23565477
Rahul Agarwal <rahulagarwal@Rahuls-MacBook-Pro.local>
2025-10-16 22:58:29 +0800

    add authentication to collaboration service

--- collaboration-service/server.js
+++ collaboration-service/server.js
@@ -1,8 +1,11 @@
+import 'dotenv/config'
 import express from 'express';
 import collaborationRoutes from './routes/collaboration-routes.js';
+import { errorHandler } from './middleware/error.middleware.js';
 
 const app = express();
 app.use(express.json());
 app.use('/api/collaboration', collaborationRoutes);
+app.use(errorHandler)
 
 export default app;
