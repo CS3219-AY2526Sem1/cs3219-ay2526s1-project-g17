bbde7f72f93f2f6217b0dc098b47f52f95eed534
Xingye Zhou <xingyechou@gmail.com>
2025-10-23 06:14:58 +0800

    feat: modify the model to accomodate the chat history

--- collaboration-service/model/session-model.js
+++ collaboration-service/model/session-model.js
@@ -1,10 +1,17 @@
 import mongoose from 'mongoose';
 
 
+const messageSchema = new mongoose.Schema({
+    userId: { type: String, required: true },
+    message: { type: String, required: true },
+    timestamp: { type: Date, default: Date.now },
+}, { _id: false });
+
 const sessionSchema = new mongoose.Schema({
   sessionId: String,
   users: [String],
   questionId: String,
+  chatHistory: [messageSchema],
   isActive: Boolean,
   createdAt: Date,
   endedAt: Date
