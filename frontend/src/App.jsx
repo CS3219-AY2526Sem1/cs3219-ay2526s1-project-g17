60c151931c2aee69bb4485813b537907e7100bfb
Rahul Agarwal <rahulagarwal@Rahuls-MacBook-Pro.local>
2025-10-23 20:22:51 +0800

    Add UX for Home Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,11 @@ import './App.css'
 import Profile from './components/Profile';
 import LoginButton from './components/LoginButton';
 import LogoutButton from './components/LogoutButton';
-import HomePage from './pages/HomePage';
+import HomePage from './pages/HomePage/HomePage.jsx';
 import { Routes, Route } from 'react-router-dom';
 import CollabPage from './collaboration/index.jsx';
 import MatchingPage from './matching-service/pages/matching_page.jsx'
+import { NavigationBar } from './components/NavigationBar.jsx';
 
 function App()  {
     return (
@@ -14,6 +15,7 @@ function App()  {
                 <Route path="/" element={<HomePage />} />
                 <Route path="/collaboration/:sessionId" element={<CollabPage />} />
                 <Route path="/match" element={<MatchingPage />} />
+                <Route path="/test" element={<NavigationBar />} />
             </Routes>
         </>
     );
