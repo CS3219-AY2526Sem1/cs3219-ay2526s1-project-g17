6b82f824c53330189726b38a5d4f75c3129f4aa2
ywllowsensor <isyazani@gmail.com>
2025-10-24 11:00:21 +0800

    feat: index difficulty and topic field to optimise randomQuestion controller

--- question_service/models/Question.js
+++ question_service/models/Question.js
@@ -27,6 +27,9 @@ const questionSchema = new mongoose.Schema(
     { timestamps: true }
 );
 
+// index for finding a random question by difficulty and topics
+questionSchema.index({ difficulty: 1, topics: 1 });
+
 const Question = mongoose.model("Question", questionSchema);
 
 export default Question;
