73ef927fe57ade82a44fb2151c5161d1ba148ec1
MinkhantEain <khantkyaw.pkk@gmail.com>
2025-10-29 21:26:24 +0800

    fix routes

--- frontend/src/matching-service/constants.js
+++ frontend/src/matching-service/constants.js
@@ -4,5 +4,12 @@ export const MATCH_FOUND = "match-found";
 export const MATCH_TIMEOUT = "match-timeout";
 export const MATCH_CANCELLED = "match-cancel";
 export const ACK = "ack";
-export const MATCHING_SERVICE_URL = "http://localhost:3001";
-export const QUESTION_SERVICE_URL = "http://localhost:5001";
+export const ENV = import.meta.env.VITE_ENV;
+export const MATCHING_SERVICE_URL =
+  ENV === "PROD"
+    ? "https://matching-service-226307456137.asia-southeast1.run.app"
+    : "http://localhost:3001";
+export const QUESTION_SERVICE_URL =
+  ENV === "PROD"
+    ? "https://question-service-226307456137.asia-southeast1.run.app"
+    : "http://localhost:5001";
