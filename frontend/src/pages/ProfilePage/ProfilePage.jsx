d2a7983ad3b7f2fab6ad5542c3d03086b0fdc968
rahula1008 <e1136941@u.nus.edu>
2025-10-30 19:20:28 +0800

    fix: fix import for nav bar

--- frontend/src/pages/ProfilePage/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage/ProfilePage.jsx
@@ -1,4 +1,5 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import { NavigationBar } from "../../components/NavigationBar/NavigationBar";
 
 export default function ProfilePage() {
   const { isAuthenticated } = useAuth0();
