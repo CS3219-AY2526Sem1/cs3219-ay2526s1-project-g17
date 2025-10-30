b43c39398e077f12c165f43f4ee21324b40e841a
rahula1008 <e1136941@u.nus.edu>
2025-10-30 19:26:48 +0800

    feat: add route for profile page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import HomePage from './pages/HomePage/HomePage.jsx';
 import { Routes, Route } from 'react-router-dom';
 import CollabPage from './collaboration/index.jsx';
 import MatchingPage from './matching-service/pages/matching_page.jsx'
+import ProfilePage from './pages/ProfilePage/ProfilePage.jsx';
 
 function App()  {
     return (
@@ -11,6 +12,7 @@ function App()  {
                 <Route path="/" element={<HomePage />} />
                 <Route path="/collaboration/:sessionId" element={<CollabPage />} />
                 <Route path="/match" element={<MatchingPage />} />
+                <Route path="/profile" element={<ProfilePage />} />
             </Routes>
         </>
     );
