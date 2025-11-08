c6b611e5171aa4474970e452d17b6bdce3419590
Charlesl12 <charles-1614@hotmail.com>
2025-11-09 01:24:58 +0800

    builld: add cors

--- collaboration-service/server.js
+++ collaboration-service/server.js
@@ -4,6 +4,14 @@ import collaborationRoutes from './routes/collaboration-routes.js';
 import { errorHandler } from './middleware/error.middleware.js';
 
 const app = express();
+
+app.use(cors({
+    origin: 'https://frontend-226307456137.asia-southeast1.run.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+}));
+
 app.use(express.json());
 app.use('/api/collaboration', collaborationRoutes);
 app.use(errorHandler)
